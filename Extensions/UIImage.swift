d464c683f661241500d5b39331e89e54f6779891
Rajat Jain <rajat.jain@aryavratinfotech.com>
2017-08-17 10:31:27 +0530

    added fixImageOrientation extension

--- Extensions/UIImage.swift
+++ Extensions/UIImage.swift
@@ -1,24 +1,47 @@
 //
 //  UIImage.swift
-//  LekhaInk
+//  SwiftAmplifier
 //
-//  Created by user on 12/07/17.
-//  Copyright © 2017 arya. All rights reserved.
+//  Created by Rajat Jain on 26/07/17.
+//  Copyright © 2017 rajatjain4061. All rights reserved.
+//  Fork this repo on Github: https://github.com/rajatjain4061/SwiftAmplifier
 //
 
 import Foundation
 import UIKit
 
 
-extension UIImageView {
-    
-    //created by Rajat Jain on 2017-07-12 to resize imageview to a fit width keeping its aspect height
-    func resizeToWidth(_ newWidth:CGFloat){
-        let width = self.frame.size.width
-        let height = self.frame.size.height
-        let newHeight = (height * 300) / width
-        
-        self.frame.size = CGSize(width: newWidth, height: newHeight)
+extension UIImage {
+    /**
+     Convert an UIview to an image
+     
+    */
+    convenience init(_ view: UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: (image?.cgImage)!)
     }
     
+    /**
+     fix image is rotated after uploading to server
+     https://stackoverflow.com/questions/5427656/ios-uiimagepickercontroller-result-image-orientation-after-upload
+     
+    */
+    func fixOrientation() -> UIImage {
+        if (self.imageOrientation == .up) {
+            return self
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        self.draw(in: rect)
+        
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return normalizedImage
+    }
 }
+
