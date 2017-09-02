3edd82e701e7f9205ad177e8c511b3046f9f4a3f
Rajat Jain <rajat.jain@aryavratinfotech.com>
2017-09-02 17:38:53 +0530

    added extension to convert UIColor to hexCode

--- Extensions/UIColor.swift
+++ Extensions/UIColor.swift
@@ -69,4 +69,18 @@ extension UIColor {
         self.init(red: (CGFloat(r)/255),green: (CGFloat(g)/255),blue: (CGFloat(b)/255),alpha:alpha)
         
     }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        
+        return NSString(format:"#%06x", rgb) as String
+    }
+    
 }
